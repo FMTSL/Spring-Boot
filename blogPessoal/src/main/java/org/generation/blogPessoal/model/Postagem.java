c9870b97f087fcd51452e990c8e4cd6aec31340e
Felipe Matos <88333095+FMTSL@users.noreply.github.com>
2023-03-10 22:54:31 -0300

    Repository

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -2,15 +2,16 @@ package org.generation.blogPessoal.model;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 
 
